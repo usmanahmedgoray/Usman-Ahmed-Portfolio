cd09674478086c9c11982b1d682e37d1f98130e6
usmanahmedgoray <usmanahmedkharal@gmail.com>
2024-09-24 22:00:56 +0500

    add the Web3.0 Projects

--- pages/Projects.js
+++ pages/Projects.js
@@ -1,69 +1,204 @@
-import { FeaturedProject1, Project1, Project2, Project3, Project4, Project5, Project6 } from '@/public/Images/Projects'
-import { motion } from 'framer-motion'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import AnimatedText from './Component/AnimatedText'
-import TransitionEffect from './Component/TransitionEffect'
-import Layout from './Layout'
+import {
+  FeaturedProject1,
+  Project1,
+  Project2,
+  Project3,
+  Project4,
+  Project5,
+  Project6,
+} from "@/public/Images/Projects";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import AnimatedText from "./Component/AnimatedText";
+import TransitionEffect from "./Component/TransitionEffect";
+import Layout from "./Layout";
+import React from "react";
+import { FeaturedProject01, Project01, Project02, Project03, Project04, Project05, Project06 } from "@/public/Images/Projects/Web3";
 
-const FramerImage = motion(Image)
+const FramerImage = motion(Image);
+
+const web2Projects = [
+  {
+    id: 1,
+    url: "https://www.skillmatch.tech/",
+    title: "SkillMatch - Advance Pre-screening Platform",
+    type: "Featured Project",
+    image: Project1,
+  },
+  {
+    id: 2,
+    url: "https://purge-ai.vercel.app/",
+    title: "Purge AI - Multi-AI Tools",
+    type: "Featured project",
+    image: Project2,
+  },
+  {
+    id: 3,
+    url: "https://seller-sync.vercel.app/",
+    title: "SyncSeller - Amazon FBA Ecommerce Wholesale Platform",
+    type: "Featured project",
+    image: Project3,
+  },
+  {
+    id: 4,
+    url: "https://te-konecta-new.vercel.app/",
+    title: "Te-Konecta - Unified Multi-Purpose All In One Link Platform",
+    type: "Featured project",
+    image: Project4,
+  },
+  {
+    id: 5,
+    url: "https://toycove-two.vercel.app/",
+    title: "Toys Cove - Ecommerce Platform for Especially Kids Products",
+    type: "Featured project",
+    image: Project5,
+  },
+  {
+    id: 6,
+    url: "https://meldasolutions.octalooptechnologies.com/",
+    title: "Melda Solutions - Ecommerce for Dacoration Related Products",
+    type: "Featured project",
+    image: Project6,
+  },
+];
+const web3Projects = [
+  {
+    id: 1,
+    url: "https://blocknotes-new.vercel.app/",
+    title: "BlockNotes - Web3 NFT as Music Industry",
+    type: "Featured Project",
+    image: Project01,
+  },
+  {
+    id: 2,
+    url: "https://lighthouse-tau.vercel.app/",
+    title: "Lighthouse Keeper- Art based NFT Dapp",
+    type: "Featured project",
+    image: Project02,
+  },
+  {
+    id: 3,
+    url: "https://gemlaunch.io/",
+    title: "GemLaunch - Decentralized Token Sales Simplified",
+    type: "Featured project",
+    image: Project03,
+  },
+  {
+    id: 4,
+    url: "https://metamuse.vercel.app/",
+    title: "MetaMuse - Solana NFT MARKETPLACE & NFT Tokens Utility",
+    type: "Featured project",
+    image: Project04,
+  },
+  {
+    id: 5,
+    url: "https://astrality.vercel.app/",
+    title: "Astrality - AI Powered Metaverse & Marketplace",
+    type: "Featured project",
+    image: Project05,
+  },
+  {
+    id: 6,
+    url: "https://nebula-skyswap-six.vercel.app/",
+    title: "Nebula SkySwap - Nebula Blockchain DEX Dapp",
+    type: "Featured project",
+    image: Project06,
+  },
+];
 
 const Featuredprojects = ({ type, title, summary, img, link }) => {
   return (
-    <article className='w-full flex justify-between items-center rounded-3xl border border-solid relative border-dark bg-light shadow-2xl p-12 rounded-br-2xl dark:bg-dark dark:text-light dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4'>
-      <div className='absolute top-0 -right-3 -z-10 w-[101%] rounded-[2.5rem] rounded-br-2xl h-[102%] bg-dark dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
-      <Link href={link} target="_blank" className='w-1/2 lg:w-full cursor-pointer overflow-hidden rounded-lg dark:bg-dark dark:text-light'>
-        <FramerImage src={img} alt={title} className='w-full h-auto'
+    <article className="w-full flex justify-between items-center rounded-3xl border border-solid relative border-dark bg-light shadow-2xl p-12 rounded-br-2xl dark:bg-dark dark:text-light dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] rounded-[2.5rem] rounded-br-2xl h-[102%] bg-dark dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 lg:w-full cursor-pointer overflow-hidden rounded-lg dark:bg-dark dark:text-light"
+      >
+        <FramerImage
+          src={img}
+          alt={title}
+          className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
           priority
           sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,50vw"
         />
       </Link>
-      <div className='w-1/2 flex flex-col items-start justify-between pl-16 lg:w-full lg:pl-0 lg:pt-6'>
-        <span className='text-primary dark:text-priaryDark font-medium text-xl xs:text-base'>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-16 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary dark:text-priaryDark font-medium text-xl xs:text-base">
           {type}
         </span>
-        <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-          <h2 className='my-2 w-full text-left text-4xl font-bold sm:text-xl'>{title}</h2>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold sm:text-xl">
+            {title}
+          </h2>
         </Link>
-        <p className='my-2 font-medium text-dark/75 dark:text-light/75'>{summary}</p>
-        <div className='mt-2 flex items-center sm:justify-between sm:w-full'>
+        <p className="my-2 font-medium text-dark/75 dark:text-light/75">
+          {summary}
+        </p>
+        <div className="mt-2 flex items-center sm:justify-between sm:w-full">
           {/* <Link href={github} target="_blank" className='w-10'>
             <BsGithub className='text-2xl' />
           </Link> */}
-          <Link href={link} target="_blank" className='rounded-lg border border-solid bg-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-light p-2 px-6 text-lg font-semibold hover:bg-light hover:border-dark hover:text-dark sm:px-4 sm:text-base' >
+          <Link
+            href={link}
+            target="_blank"
+            className="rounded-lg border border-solid bg-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-light p-2 px-6 text-lg font-semibold hover:bg-light hover:border-dark hover:text-dark sm:px-4 sm:text-base"
+          >
             Visit Project
           </Link>
         </div>
       </div>
     </article>
-  )
-}
+  );
+};
 
-const Project = ({ type, title,img, link }) => {
+const Project = ({ type, title, img, link }) => {
   return (
     <>
-      <article className='w-full flex min-h-[485px] flex-col justify-start items-center rounded-3xl border border-solid relative border-dark bg-light shadow-2xl p-6 rounded-br-2xl dark:bg-dark dark:text-light dark:border-light xs:p-4'>
-        <div className='absolute top-0 -right-3 -z-10 w-[101%] rounded-[2.5rem] rounded-br-2xl h-[102%] bg-dark dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]' />
-        <Link href={link} target="_blank" className='cursor-pointer overflow-hidden rounded-lg dark:bg-dark dark:text-light'>
-          <FramerImage src={img} alt={title} className='w-full h-auto'
+      <article className="w-full flex min-h-[485px] flex-col justify-start items-center rounded-3xl border border-solid relative border-dark bg-light shadow-2xl p-6 rounded-br-2xl dark:bg-dark dark:text-light dark:border-light xs:p-4">
+        <div className="absolute top-0 -right-3 -z-10 w-[101%] rounded-[2.5rem] rounded-br-2xl h-[102%] bg-dark dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]" />
+        <Link
+          href={link}
+          target="_blank"
+          className="cursor-pointer overflow-hidden rounded-lg dark:bg-dark dark:text-light"
+        >
+          <FramerImage
+            src={img}
+            alt={title}
+            className="w-full h-auto"
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.2 }}
             priority
             sizes="(max-width:768px) 100vw,(max-width:1200px) 50vw,50vw"
           />
         </Link>
-        <div className='flex flex-col items-start justify-between mt-5'>
-          <span className='text-primary dark:text-priaryDark font-medium text-xl lg:text-lg sm:text-base'>
+        <div className="flex flex-col items-start justify-between mt-5">
+          <span className="text-primary dark:text-priaryDark font-medium text-xl lg:text-lg sm:text-base">
             {type}
           </span>
-          <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
-            <h2 className='my-2 w-full text-left text-4xl font-bold lg:text-3xl sm:text-xl'>{title}</h2>
+          <Link
+            href={link}
+            target="_blank"
+            className="hover:underline underline-offset-2"
+          >
+            <h2 className="my-2 w-full text-left text-4xl font-bold lg:text-3xl sm:text-xl">
+              {title}
+            </h2>
           </Link>
-          <div className='mt-2 flex items-center sm:justify-between sm:w-full'>
-            <Link href={link} target="_blank" className='mr-4 rounded-lg border border-solid hover:bg-light hover:border-dark hover:text-dark bg-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-light p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base' >
+          <div className="mt-2 flex items-center sm:justify-between sm:w-full">
+            <Link
+              href={link}
+              target="_blank"
+              className="mr-4 rounded-lg border border-solid hover:bg-light hover:border-dark hover:text-dark bg-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light text-light p-2 px-6 text-lg font-semibold sm:px-4 sm:text-base"
+            >
               Visit Project
             </Link>
             {/* <Link href={github} target="_blank" className='w-10 sm:w-8'>
@@ -71,25 +206,35 @@ const Project = ({ type, title,img, link }) => {
             </Link> */}
           </div>
         </div>
-      </article >
+      </article>
     </>
-  )
-}
+  );
+};
 
 const Projects = () => {
   return (
     <>
       <Head>
         <title>Usman Ahmed | Projects</title>
-        <meta name='description' content='This is Usman Ahmed Porfolio next app project page' />
+        <meta
+          name="description"
+          content="This is Usman Ahmed Porfolio next app project page"
+        />
       </Head>
-      <TransitionEffect/>
-      <main className='w-full mb-16 flex flex-col justify-center items-center dark:bg-dark dark:text-light'>
-        <Layout className='pt-16'>
-          <AnimatedText text="Imagination Trumps Knowledge!" className='text-8xl mb-16 lg:text-7xl sm:text-6xl sm:mb-8 xs:text-4xl' />
-          <AnimatedText text="WEB 2.0 PROJECTS" className='text-6xl text-left ml-3 mt-16 mb-8 lg:text-5xl sm:text-4xl sm:mb-8 xs:text-4xl text-primary dark:text-priaryDark' />
-          <div className='grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 xs:gap-x-0 dark:text-light'>
-            <div className='col-span-12'>
+      <TransitionEffect />
+      <main className="w-full mb-16 flex flex-col justify-center items-center dark:bg-dark dark:text-light">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="Imagination Trumps Knowledge!"
+            className="text-8xl mb-16 lg:text-7xl sm:text-6xl sm:mb-8 xs:text-4xl"
+          />
+          <AnimatedText
+            text="WEB 2.0 PROJECTS"
+            className="text-6xl text-left ml-3 mt-16 mb-8 lg:text-5xl sm:text-4xl sm:mb-8 xs:text-4xl text-primary dark:text-priaryDark"
+          />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 xs:gap-x-0 dark:text-light">
+            
+            <div className="col-span-12">
               <Featuredprojects
                 title="Web3Geeks"
                 summary="An online learning platform similar to Udemy, with added blockchain features.Currently working on both front-end and back-end development, developing and integrating blockchain technology to enable course enrollment and other unique features."
@@ -99,84 +244,62 @@ const Projects = () => {
                 img={FeaturedProject1}
               />
             </div>
-            <div className='col-span-6 sm:col-span-12'>
-              <Project
-                title="SkillMatch - Advance Pre-screening Platform"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
+            {web2Projects &&
+              web2Projects?.map((project) => {
+                return (
+                  <div key={project.id} className="col-span-6 sm:col-span-12">
+                    <Project
+                      title={project.title}
+                      summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
               page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://www.skillmatch.tech/"
-                img={Project1}
-                type="Featured project"
-
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-12'>
-            <Project
-                title="Purge AI - Multi-AI Tools"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
-              page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://purge-ai.vercel.app/"
-                img={Project2}
-                type="Featured project"
-
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-12'>
-            <Project
-                title="SyncSeller - Amazon FBA Ecommerce Wholesale Platform"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
-              page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://seller-sync.vercel.app/"
-                img={Project3}
-                type="Featured project"
-
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-12'>
-            <Project
-                title="Te-Konecta - Unified Multi-Purpose All In One Link Platform"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
-              page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://te-konecta-new.vercel.app/"
-                img={Project4}
-                type="Featured project"
-
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-12'>
-            <Project
-                title="Toys Cove - Ecommerce Platform for Especially Kids Products"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
-              page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://toycove-two.vercel.app/"
-                img={Project5}
-                type="Featured project"
-
-              />
-            </div>
-            <div className='col-span-6 sm:col-span-12'>
-            <Project
-                title="Melda Solutions - Ecommerce for Dacoration Related Products"
-                summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
-              page transitions, cool background effects, unique design and it is mobile responsive."
-                // github="https://www.github.com/usmanahmedgoray"
-                link="https://meldasolutions.octalooptechnologies.com/"
-                img={Project6}
-                type="Featured project"
-
-              />
-            </div>
+                      // github="https://www.github.com/usmanahmedgoray"
+                      link={project.url}
+                      img={project.image}
+                      type="Featured project"
+                    />
+                  </div>
+                );
+              })}
+            
           </div>
-          
+          <AnimatedText
+            text="WEB 3.0 PROJECTS"
+            className="text-6xl text-left ml-3 mt-16 mb-8 lg:text-5xl sm:text-4xl sm:mb-8 xs:text-4xl text-primary dark:text-priaryDark"
+          />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 xs:gap-x-0 dark:text-light">
+            
+            <div className="col-span-12">
+              <Featuredprojects
+                title="The Moonbase"
+                summary="MoonbaseAlpha is not only uniquely decentralized in it’s Launchpad construct, it also contains the direct vessel to launch via a high velocity rocket charged AMM Exchange. This entire MoonbaseAlpha solution is powered by Arbitrum & zkSync Protocol."
+                link="https://web3-geeks-beige.vercel.app"
+                // github="https://github.com/usmanahmedgoray/Alan-AI-Voice-News-Assistant"
+                type={"Featured Project"}
+                img={FeaturedProject01}
+              />
+            </div>
+            {web3Projects &&
+              web3Projects?.map((project) => {
+                return (
+                  <div key={project?.id} className="col-span-6 sm:col-span-12">
+                    <Project
+                      title={project?.title}
+                      summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth 
+              page transitions, cool background effects, unique design and it is mobile responsive."
+                      // github="https://www.github.com/usmanahmedgoray"
+                      link={project.url}
+                      img={project.image}
+                      type="Featured project"
+                    />
+                  </div>
+                );
+              })}
+            
+          </div>
         </Layout>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
---
+++ public/Images/Projects/Web3/index.js
@@ -0,0 +1,17 @@
+import Project02 from "./LightHouse.png";
+import FeaturedProject01 from './theMoonbase.png';
+import Project06 from "./Nebula-skyswap.png";
+import Project01 from "./BlackNotes.png";
+import Project03 from "./Gemlaunch.png";
+import Project04 from "./MetaMuse.png";
+import Project05 from "./Astrality.png";
+
+export {
+    FeaturedProject01,
+    Project01,
+    Project02,
+    Project03,
+    Project04,
+    Project05,
+    Project06
+};
