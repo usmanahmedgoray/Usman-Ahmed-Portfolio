856be8df3843ff7a874a2a6b4e0c574b46a888cc
usmanahmedgoray <usmanahmedkharal@gmail.com>
2024-09-25 15:27:14 +0500

    Add the updated resume

--- pages/index.js
+++ pages/index.js
@@ -28,10 +28,10 @@ const index = () => {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
               <Animatedtext text="Turning Vision Into Reality With Code And Design" className='text-6xl text-left xl:text-5xl lg:text-center lg:text-6xl md:textt-5xl sm:text-3xl ' />
-              <p className='my-4 text-base font-medium text-gray-800 dark:text-light md:text-sm sm:text-xs'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
-                Explore my latest projects and articles, showcasing my expertise in React.js and web development</p>
+              <p className='my-4 text-base font-medium text-gray-800 dark:text-light md:text-sm sm:text-xs'>As a skilled full-stack Blockchain and Web developer, I am dedicated to turning ideas into innovative Web Applications, DeFi,DAO and NFT Dapps and Solidity Smart Contracts and Solana Blockchain programs .
+                Explore my latest projects and articles, showcasing my expertise in Blockchain and Web Development</p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
-                <Link href="/UsmanResume.pdf" target='_blank' className='flex items-center bg-dark text-light py-2.5 px-6 rounded-lg border border-solid border-transparent hover:border-dark hover:bg-light hover:text-dark active:bg-dark active:text-light dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:px-4 md:p-2 md:text-base'>
+                <Link href="/UsmanAhmedGoraya.pdf" target='_blank' className='flex items-center bg-dark text-light py-2.5 px-6 rounded-lg border border-solid border-transparent hover:border-dark hover:bg-light hover:text-dark active:bg-dark active:text-light dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:px-4 md:p-2 md:text-base'>
                   Resume <BsBoxArrowUpRight className='text-xl ml-3' />
                 </Link>
                 <Link href="mailto:usmanahmedkharal@gmail.com" className='ml-4 capitalize font-medium underline underline-offset-2 text-lg md:text-base'>
