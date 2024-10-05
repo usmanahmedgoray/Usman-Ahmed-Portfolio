b000f5eeb3ddba72212cf9f315166cf2d7156bab
usmanahmedgoray <usmanahmedkharal@gmail.com>
2024-10-05 10:34:42 +0500

    Overflow hidden issue resolve

--- pages/Component/HireMe.js
+++ pages/Component/HireMe.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const HireMe = () => {
   return (
-    <div className='fixed left-20 bottom-24 flex items-center justify-center md:right-16 md:left-auto md:top-12 md:bottom-auto md:absolute overflow-hidden'>
-        <div className='w-44 absolute h-[10rem] flex items-center justify-center md:w-24'>
+    <div className='fixed left-20 bottom-24 flex items-center justify-center md:right-16 md:left-auto md:top-12 md:bottom-auto md:absolute '>
+        <div className='w-44 absolute h-[10rem] flex items-center justify-center overflow-hidden md:w-24'>
             <Icons className={"fill-dark animate-spin-slow dark:fill-light"}/>
             <Link href="http://www.linkedin.com/in/usmanahmedgoray"  target="_blank" className='flex justify-center items-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark dark:bg-light dark:text-dark text-light border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark shadow-md hover:dark:text-light hover:dark:bg-dark hover:dark:border-light md:w-12 md:h-12 md:text-[10px]'>
                 Hire Me
